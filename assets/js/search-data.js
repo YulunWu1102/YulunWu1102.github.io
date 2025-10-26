230ec6999616fb1afc92698ea480d9b5258ac646
YulunWu1102 <90520085+YulunWu1102@users.noreply.github.com>
2025-10-26 04:55:07 +0000

    Deploying to gh-pages from @ YulunWu1102/YulunWu1102.github.io@8a24390452943de8f0ea0469e5a3407796e67093 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -459,36 +459,6 @@ ninja.data = [{
           description: "2024 — ECE 513 final project on beamforming, musical-noise reduction, and subspace PCA variants",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
             },},{
         id: 'social-email',
         title: 'email',
